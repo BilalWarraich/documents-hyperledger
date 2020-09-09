61b073f284c0e7b35c12085d0b4bbb5a8c25acb8
BilalWarraich <bilalahmad947@gmail.com>
2020-09-09 17:42:09 +0500

    first commit

---
+++ fabric/chaincode/verify/go/verify.go
@@ -0,0 +1,282 @@
+/*
+ SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/hyperledger/fabric-protos-go/peer"
+)
+
+//SmartContract is the data structure which represents this contract and on which  various contract lifecycle functions are attached
+type SmartContract struct {
+}
+
+type Document struct {
+	ObjectType string `json:"Type"`
+	Document   string `json:"document"`
+	Hash       string `json:"hash"`
+}
+
+type Admin struct {
+	ObjectType string `json:"Type"`
+	AdminID    string `json:"adminID"`
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+}
+
+func (t *SmartContract) Init(stub shim.ChaincodeStubInterface) peer.Response {
+
+	fmt.Println("Init Firing!")
+	return shim.Success(nil)
+}
+
+func (t *SmartContract) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+
+	// Retrieve the requested Smart Contract function and arguments
+	function, args := stub.GetFunctionAndParameters()
+	fmt.Println("Chaincode Invoke Is Running " + function)
+	if function == "addDocument" {
+		return t.addDocument(stub, args)
+	}
+	if function == "queryDocuments" {
+		return t.queryDocuments(stub)
+	}
+	if function == "queryDocumentByHash" {
+		return t.queryDocumentByHash(stub, args)
+	}
+	if function == "addAdmin" {
+		return t.addAdmin(stub, args)
+	}
+	if function == "queryAdmin" {
+		return t.queryAdmin(stub, args)
+	}
+
+	fmt.Println("Invoke did not find specified function " + function)
+	return shim.Error("Invoke did not find specified function " + function)
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+func (t *SmartContract) addDocument(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	var err error
+
+	if len(args) != 1 {
+		return shim.Error("Incorrect Number of Aruments. Expecting 1")
+	}
+
+	fmt.Println("Adding new Documnet")
+
+	// ==== Input sanitation ====
+	if len(args[0]) <= 0 {
+		return shim.Error("1st Argument Must be a Non-Empty String")
+	}
+	document := args[0]
+
+	rand.Seed(time.Now().UnixNano())
+	hash := randSeq(30)
+
+	// ======Check if Document Already exists
+
+	DocumentAsBytes, err := stub.GetState(hash)
+	if err != nil {
+		return shim.Error("Transaction Failed with Error: " + err.Error())
+	} else if DocumentAsBytes != nil {
+		return shim.Error("The Inserted document already Exists")
+	}
+
+	// ===== Create Document Object and Marshal to JSON
+
+	objectType := "Document"
+	Document := &Document{objectType, document, hash}
+	DocumentJSONasBytes, err := json.Marshal(Document)
+
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Save Document to State
+
+	err = stub.PutState(document, DocumentJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Return Success
+
+	fmt.Println("Successfully Saved Document")
+	return shim.Success(nil)
+}
+
+func (t *SmartContract) queryDocumentByHash(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	if len(args) < 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	hash := args[0]
+
+	queryString := fmt.Sprintf("{\"selector\":{\"Type\":\"Document\",\"hash\":\"%s\"}}", hash)
+
+	queryResults, err := getQueryResultForQueryString(stub, queryString)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(queryResults)
+}
+
+func (t *SmartContract) queryDocuments(stub shim.ChaincodeStubInterface) peer.Response {
+
+	queryString := fmt.Sprintf("{\"selector\":{\"Type\":\"Document\"}}")
+
+	queryResults, err := getQueryResultForQueryString(stub, queryString)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(queryResults)
+}
+
+func (t *SmartContract) addAdmin(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	var err error
+
+	if len(args) != 3 {
+		return shim.Error("Incorrect Number of Aruments. Expecting 3")
+	}
+
+	fmt.Println("Adding new Admin")
+
+	// ==== Input sanitation ====
+	if len(args[0]) <= 0 {
+		return shim.Error("1st Argument Must be a Non-Empty String")
+	}
+	if len(args[1]) <= 0 {
+		return shim.Error("2nd Argument Must be a Non-Empty String")
+	}
+	if len(args[2]) <= 0 {
+		return shim.Error("3rd Argument Must be a Non-Empty String")
+	}
+
+	adminID := args[0]
+	username := args[1]
+	password := args[2]
+
+	// ======Check if admin Already exists
+
+	adminAsBytes, err := stub.GetState(adminID)
+	if err != nil {
+		return shim.Error("Transaction Failed with Error: " + err.Error())
+	} else if adminAsBytes != nil {
+		return shim.Error("The Inserted expert ID already Exists: " + adminID)
+	}
+
+	// ===== Create admin Object and Marshal to JSON
+
+	objectType := "admin"
+	admin := &Admin{objectType, adminID, username, password}
+	adminJSONasBytes, err := json.Marshal(admin)
+
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Save admin to State
+
+	err = stub.PutState(adminID, adminJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Return Success
+
+	fmt.Println("Successfully Saved admin")
+	return shim.Success(nil)
+}
+
+func (t *SmartContract) queryAdmin(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	if len(args) < 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	username := args[0]
+	password := args[1]
+
+	queryString := fmt.Sprintf("{\"selector\":{\"Type\":\"admin\",\"username\":\"%s\",\"password\":\"%s\"}}", username, password)
+
+	queryResults, err := getQueryResultForQueryString(stub, queryString)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(queryResults)
+}
+
+func getQueryResultForQueryString(stub shim.ChaincodeStubInterface, queryString string) ([]byte, error) {
+
+	fmt.Printf("- getQueryResultForQueryString queryString:\n%s\n", queryString)
+
+	resultsIterator, err := stub.GetQueryResult(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing QueryRecords
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Record\":")
+		// Record is a JSON object, so we write as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	fmt.Printf("- getQueryResultForQueryString queryResult:\n%s\n", buffer.String())
+
+	return buffer.Bytes(), nil
+}
+
+//Main Function starts up the Chaincode
+func main() {
+	err := shim.Start(new(SmartContract))
+	if err != nil {
+		fmt.Printf("Smart Contract could not be run. Error Occured: %s", err)
+	} else {
+		fmt.Println("Smart Contract successfully Initiated")
+	}
+}
---
+++ fabric/test-network/installChaincode.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export PATH=${PWD}/../bin:$PATH
+
+export FABRIC_CFG_PATH=$PWD/../config/
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+peer lifecycle chaincode package verify.tar.gz --path ../chaincode/verify/go/ --lang golang --label verify_1
+peer lifecycle chaincode install verify.tar.gz
+
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt
+export CORE_PEER_ADDRESS=localhost:8051
+peer channel join -b ${PWD}/channel-artifacts/mychannel.block
+peer lifecycle chaincode install verify.tar.gz
+
+
+
+peer lifecycle chaincode queryinstalled
+
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+
+export CORE_PEER_ADDRESS=localhost:7051
+
+
+
